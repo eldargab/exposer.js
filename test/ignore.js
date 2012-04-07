d3592880409eb895ba43d354b6029610c92b97e8
Eldar Gabdullin <eldargab@gmail.com>
2012-04-07 19:56:53 +0400

    Refactor everything. Added: support for bundles, ignoring with globs

---
+++ test/ignore.js
@@ -0,0 +1,29 @@
+var Ignore = require('../lib/ignore')
+
+function test (pattern, path) {
+    var i = new Ignore([pattern], '/')
+    var isDir = false
+    path = path.replace(/\/$/, function () {
+        isDir = true
+        return ''
+    })
+    var matched = i.match(path, isDir)
+    return {
+        matched: function () {
+            matched.should.be.true
+        },
+        unmatched: function () {
+            matched.should.be.false
+        }
+    }
+}
+
+describe('Ignore', function () {
+    it('test', function () {
+        test('bin/', 'hello/bin/').matched()
+        test('/bin/', 'hello/bin/').unmatched()
+        test('/bin/', '/bin/').matched()
+        test('/bin/', '/bin').unmatched()
+        test('*.json', '/package.json').matched()
+    })
+})
